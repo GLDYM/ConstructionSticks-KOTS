9a51ee46795b665d14655e5dea57224f81be19d1
Polaris_Light <995905922@qq.com>
2026-02-17 17:26:28 +0800

    feat(container): ae2

--- src/main/java/mrbysco/constructionstick/containers/ContainerRegistrar.java
+++ src/main/java/mrbysco/constructionstick/containers/ContainerRegistrar.java
@@ -3,7 +3,11 @@ package mrbysco.constructionstick.containers;
 import mrbysco.constructionstick.ConstructionStick;
 import mrbysco.constructionstick.containers.handlers.HandlerBundle;
 import mrbysco.constructionstick.containers.handlers.HandlerCapability;
+// import mrbysco.constructionstick.containers.handlers.HandlerDimensionsNet;
+// import mrbysco.constructionstick.containers.handlers.HandlerNetTerminal;
+import mrbysco.constructionstick.containers.handlers.HandlerPortableCell;
 import mrbysco.constructionstick.containers.handlers.HandlerShulkerbox;
+import mrbysco.constructionstick.containers.handlers.HandlerWirelessTerminal;
 import net.neoforged.fml.ModList;
 public class ContainerRegistrar {
 	public static boolean isCuriosLoaded = ModList.get().isLoaded("curios");
@@ -21,5 +25,27 @@ public class ContainerRegistrar {
         if(isCuriosLoaded) {
             ConstructionStick.LOGGER.info("Curios integration added");
         }
+
+        if(ModList.get().isLoaded("ae2")) {
+            ConstructionStick.containerManager.register(new HandlerPortableCell());
+            ConstructionStick.containerManager.register(new HandlerWirelessTerminal());
+            ConstructionStick.LOGGER.info("Applied Energistics 2 integration added");
+        }
+
+        // if(ModList.get().isLoaded("toms_storage")) {
+        //     ConstructionStick.instance.containerManager.register(new HandlerAdvWirelessTerminal());
+        //     ConstructionStick.LOGGER.info("Tom's Simple Storage integration added");
+        // }
+
+        // if(ModList.get().isLoaded("refinedstorage")) {
+        //     ConstructionStick.instance.containerManager.register(new HandlerWirelessGrid());
+        //     ConstructionStick.LOGGER.info("Refined Storage integration added");
+        // }
+
+        // if(ModList.get().isLoaded("beyonddimensions")) {
+        //     ConstructionStick.containerManager.register(new HandlerDimensionsNet());
+        //     ConstructionStick.containerManager.register(new HandlerNetTerminal());
+        //     ConstructionStick.LOGGER.info("Beyond Dimensions integration added");
+        // }
 	}
 }
---
+++ src/main/java/mrbysco/constructionstick/containers/handlers/HandlerDimensionsNet.java
@@ -0,0 +1,60 @@
+// package mrbysco.constructionstick.containers.handlers;
+
+// import net.minecraft.world.entity.player.Player;
+// import net.minecraft.world.item.Item;
+// import net.minecraft.world.item.ItemStack;
+// import mrbysco.constructionstick.api.IContainerHandler;
+// import mrbysco.constructionstick.containers.ContainerTrace;
+// import mrbysco.constructionstick.items.stick.ItemStick;
+// import com.wintercogs.beyonddimensions.Api.DataBase.DimensionsNet;
+// import com.wintercogs.beyonddimensions.Api.DataBase.Storage.UnifiedStorage;
+// import com.wintercogs.beyonddimensions.Api.DataBase.Stack.IStackType;
+// import com.wintercogs.beyonddimensions.Api.DataBase.Stack.ItemStackType;
+
+// public class HandlerDimensionsNet implements IContainerHandler {
+//     // This Handler does not related with any specific Item, it works as long as the player has a Dimensions Net.
+//     // For every itemStack, matches method will return true, but the signature confirms only one net used.
+
+//     @Override
+//     public boolean matches(Player player, ItemStack inventoryStack) {
+//         if (!(inventoryStack.getItem() instanceof ItemStick)) return false;
+//         DimensionsNet net = DimensionsNet.getNetFromPlayer(player);
+//         return net != null;
+//     }
+
+//     @Override
+//     public int getSignature(Player player, ItemStack inventoryStack) {
+//         DimensionsNet net = DimensionsNet.getNetFromPlayer(player);
+//         return (net != null) ? 10000 + net.getId() : -1;
+//     }
+
+//     @Override
+//     public int countItems(Player player, ContainerTrace trace, ItemStack itemStack, ItemStack inventoryStack) {
+//         DimensionsNet net = DimensionsNet.getNetFromPlayer(player);
+//         if (net == null) return 0;
+
+//         UnifiedStorage storage = net.getUnifiedStorage();
+//         ItemStackType query = new ItemStackType(new ItemStack(itemStack.getItem(), Integer.MAX_VALUE));
+//         IStackType<?> result = storage.extract(query, true);
+
+//         if (result instanceof ItemStackType itemResult) {
+//             return itemResult.getStack().getCount();
+//         }
+//         return 0;
+//     }
+
+//     @Override
+//     public int useItems(Player player, ContainerTrace trace, ItemStack itemStack, ItemStack inventoryStack, int count) {
+//         DimensionsNet net = DimensionsNet.getNetFromPlayer(player);
+//         if (net == null) return 0;
+
+//         UnifiedStorage storage = net.getUnifiedStorage();
+//         ItemStackType request = new ItemStackType(new ItemStack(itemStack.getItem(), count));
+//         IStackType<?> extracted = storage.extract(request, false);
+
+//         if (extracted instanceof ItemStackType itemResult) {
+//             return count - itemResult.getStack().getCount();
+//         }
+//         return 0;
+//     }
+// }
